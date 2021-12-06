1cb87add6d4332d59ba858b6faf8626bd58fb61c
Mateusz Grzenia <93188703+Leksusik@users.noreply.github.com>
2021-12-06 21:19:24 +0100

    Add files via upload

---
+++ obiektowe/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace obiektowe
+{
+    class Program
+    {
+        static void Nawiasy()
+        {
+            string napis = "( ( ) )()((";
+            Stack<char> StosOtwarc = new Stack<char>();
+            char buffer;
+            /*int licznikOkraglych = 0,
+                licznikKwadratowych;*/
+            foreach (char znak in napis)
+            {
+                if (znak == '(')
+                {
+                    StosOtwarc.Push('o');
+                }
+                else if (znak == ')')
+                {
+                    if (StosOtwarc.TryPop(out buffer))
+                    {
+
+                    }
+                    else {
+                        Console.WriteLine("Niepoprawne domykanie");
+                        break; }
+                }
+            }
+            if (StosOtwarc.TryPeek(out buffer))
+            {
+                Console.WriteLine("Zostaly otwarte nawiasy");
+            }
+            else
+            {
+                Console.WriteLine("Git");
+            }
+        }
+        static void TestStosu()
+        {
+            Stack<char> Stos = new Stack<char>();
+            Stos.Push('z');
+
+            char buffer;
+            Console.WriteLine("Mozna cos zrzucic ze stosu? {0}\nZrzucona wartosc: {1}", Stos.TryPop(out buffer),buffer);
+            Console.WriteLine("Mozna cos zrzucic ze stosu? {0}\nZrzucona wartosc: {1}", Stos.TryPop(out buffer), buffer);
+        }
+        static void Main(string[] args)
+        {
+            Nawiasy();
+        }
+    }
+}
